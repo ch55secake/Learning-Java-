1779e5fd92f9e62b06ff8da5a24974f7353d8107
Oscar Serantes Mccormack <87881861+ch55secake@users.noreply.github.com>
2021-09-26 20:30:46 +0100

    Add files via upload

---
+++ src/printingAndVariables.java
@@ -0,0 +1,19 @@
+public class printingAndVariables {
+
+    public static void main(String[] args) {
+        System.out.println("I love this");
+        System.out.println("I love this ");
+
+        int x = 18299; // initialization
+        float y = 3.14f; // float means you can store decimal but needs to have f at the end
+        double xy = 3.145; // much more precise than a float and you don't need the f at the end
+        boolean z = false; // true or false
+        String zy = "@"; // store words and shit like that
+        String name = "Oscar"; // like a normal string and can be concatenated
+        String a = "me";
+        String b = "me too";
+        String temp;
+        temp = a; // long and short of this section is that you can change variables and store them elsewhere and then replace them
+    }
+
+}
